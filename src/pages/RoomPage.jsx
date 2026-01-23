506d6cf0c77979c60b0cef5165f143c023809bbc
Muhammad Achar <muhammadacharzardari12@gmail.com>
2026-01-23 18:44:33 +0500

    Update RoomPage.jsx

--- src/pages/RoomPage.jsx
+++ src/pages/RoomPage.jsx
@@ -653,6 +653,27 @@ const RoomPage = () => {
     socket.on("incoming-call", handleIncomingCall);
     
     socket.on("call-accepted", handleCallAccepted);
+
+        socket.on("chat-message", (data) => {
+      // Add to chat
+      dispatch({ type: "ADD_MESSAGE", payload: data });
+
+      // Show toast for messages from others
+      if (data.from !== socket.id) {
+        // Use data.senderName that comes from backend
+        toast.custom(
+          (t) => (
+            <div className="bg-green-800 shadow-2xl text-white p-4 rounded-xl flex items-center gap-2 z-50">
+              <MessageSquareText className="w-5 h-5" />
+              <span>
+                {data.senderName || "Guest"}: {data.text}
+              </span>
+            </div>
+          ),
+          { duration: 3000 },
+        );
+      }
+    });
     
     // user-left
     socket.on("user-left", ({ socketId }) => {
@@ -1015,7 +1036,7 @@ const RoomPage = () => {
       }
     };
 
-    socket.on("chat-message", handleChatMessage);
+    
     return () => socket.off("chat-message", handleChatMessage);
   }, [socket, state.remoteName]);
 
