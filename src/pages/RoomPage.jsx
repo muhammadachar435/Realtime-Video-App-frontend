0c1de86c8e988a93f07f27817cf4d45eabbc2002
Muhammad Achar <muhammadacharzardari12@gmail.com>
2026-01-24 23:19:34 +0500

    Update RoomPage.jsx

--- src/pages/RoomPage.jsx
+++ src/pages/RoomPage.jsx
@@ -1064,7 +1064,7 @@ const RoomPage = () => {
 
         <div
           onClick={toggleMic}
-          className={`p-3 rounded-full bg-[#364355] hover:bg-[#2e4361] cursor-pointer ${state.micOn ? "bg-gray-900" : ""} `}
+          className={`hidden md:block  p-3 rounded-full bg-[#364355] hover:bg-[#2e4361] cursor-pointer ${state.micOn ? "bg-gray-900" : ""} `}
           title="Toggle Microphone"
         >
           {state.micOn ? <Mic /> : <MicOff />}
